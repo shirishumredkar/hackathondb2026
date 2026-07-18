a9fd2e6e1a917f4f6c54fea697327174582ed0a1
Shirish Umredkar <shirish.umredkar@gmail.com>
2026-07-18 07:22:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+########################################################################################################################################################################
+## Provider Setup
+########################################################################################################################################################################
 terraform {
   required_version = ">= 1.0.0"
   backend "gcs" {
@@ -17,8 +20,24 @@ provider "google" {
   region  = "us-central1"
 }
 
-# Example resource: A simple VPC
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = true
+########################################################################################################################################################################
+## Enabling the API Services
+########################################################################################################################################################################
+resource "google_project_service" "iamcredentials" {
+  project = "272907652960"  
+  service = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
 }
+
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "iamcredentials.googleapis.com",
+    "storage.googleapis.com",
+    "compute.googleapis.com",
+  ])
+  project = "272907652960"
+  service = each.key
+
+  disable_on_destroy = false
+}
+
