e8fd2cad677906f294b294cce2d658bd9e32054c
Shirish Umredkar <shirish.umredkar@gmail.com>
2026-07-18 19:38:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,8 @@ resource "google_project_service" "required_apis" {
     "iam.googleapis.com",
     "cloudkms.googleapis.com",
     "artifactregistry.googleapis.com",
-    "run.googleapis.com"
+    "run.googleapis.com",
+    "secretmanager.googleapis.com"
   ])
   project            = var.project_id
   service            = each.key
