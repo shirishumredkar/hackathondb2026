c44df480e26d423e0b8b3d167fc3362841543725
Shirish Umredkar <shirish.umredkar@gmail.com>
2026-07-18 08:31:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
-variable "project_id" { type = string; default = "project-495bdca4-ac50-4df5-bb6" }
-variable "region"     { type = string; default = "us-central1" }
+variable "project_id" {
+  type    = string
+  default = "project-495bdca4-ac50-4df5-bb6"
+}
+variable "region"     { 
+   type = string
+   default = "us-central1" 
+}
